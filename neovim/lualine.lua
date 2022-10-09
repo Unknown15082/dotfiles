033c44c3072f1a26c6d54b2dfe22b384aa2e8d4b
Unknown15082 <trangiahuy15082006@gmail.com>
2022-10-09 23:41:33 +0700

    Shorten lualine

--- neovim/lualine.lua
+++ neovim/lualine.lua
@@ -1,5 +1,14 @@
 require('lualine').setup {
     options = {
         theme = 'codedark'
-    }
+    },
+    sections = {
+        lualine_x = {},
+        lualine_y = { 'filetype' },
+    },
+    inactive_sessions = {
+        lualine_a = { 'filename' },
+        lualine_x = {},
+    },
+    extensions = { 'nvim-tree' },
 }
